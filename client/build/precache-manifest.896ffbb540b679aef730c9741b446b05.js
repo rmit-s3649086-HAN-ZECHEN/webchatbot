c3082fc3fd0a2ab5be9bd436a2927a5e4890a096
Han <han13066022700@gmail.com>
2020-05-20 00:09:10 +1000

    6th version deploy

---
+++ client/build/precache-manifest.896ffbb540b679aef730c9741b446b05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c683b62c762cdef70d6d2b003354b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a666c21382f555612ac6",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "a666c21382f555612ac6",
+    "url": "/static/js/2.6ca99dde.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/static/js/2.6ca99dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c585f40cb224c54cb4a8",
+    "url": "/static/js/main.7f44f415.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
