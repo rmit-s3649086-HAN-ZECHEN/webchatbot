1f4ff9cd8b02bc61fd5bd1093438b82fbbf91637
Han <han13066022700@gmail.com>
2020-05-19 21:08:31 +1000

    5th version deploy debug2

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -4,9 +4,9 @@ const chatbot = require('../chatbot/chatbot');
 
 module.exports = app => {
 
-//    app.get('/', (req, res) => {
-//        res.send({'hello': 'Johnny'})
-//    });
+     app.get('/', (req, res) => {
+         res.send({'hello': 'Johnny'})
+     });
 
     app.post('/api/df_text_query', async (req, res) => {
 
@@ -19,4 +19,9 @@ module.exports = app => {
         let responses = await chatbot.eventQuery(req.body.event, req.body.userID, req.body.parameters);
         res.send(responses[0].queryResult);
     });
+
+    app.get('/api/get_client_token', async (req, res) => {
+        let token = await chatbot.getToken();
+        res.send({token});
+    })
 }
